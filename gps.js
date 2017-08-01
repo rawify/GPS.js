6d0d78b435f7a48eb1e85ab9dc1dc0d3fad0c4c4
Robert Eisele <robert@xarg.org>
2017-08-01 20:05:14 +0200

    Version++

--- gps.js
+++ gps.js
@@ -1,5 +1,5 @@
 /**
- * @license GPS.js v0.3.0 26/01/2016
+ * @license GPS.js v0.4.0 26/01/2016
  *
  * Copyright (c) 2016, Robert Eisele (robert@xarg.org)
  * Dual licensed under the MIT or GPL Version 2 licenses.
