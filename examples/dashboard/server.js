59516141c2f922b9eb4fb776a17b63b323c98fb5
Robert Eisele <robert@xarg.org>
2018-03-17 10:00:55 +0100

    Added speedometer

--- examples/dashboard/server.js
+++ examples/dashboard/server.js
@@ -6,7 +6,7 @@ const io = require('socket.io')(http);
 // var file = '/dev/cu.usbserial';
 // var file = '/dev/ttyUSB0';
 //var file = '/dev/tty.usbserial';
-const file = '/dev/tty.usbmodem1411';
+const file = '/dev/tty.usbmodem1421';
 
 const SerialPort = require('serialport');
 const parsers = SerialPort.parsers;
@@ -27,13 +27,21 @@ app.get('/', function(req, res) {
 
 var GPS = require('../../gps.js');
 var gps = new GPS;
+gps.state.bearing = 0;
+var prev = {lat: null, lon: null};
 
 http.listen(3000, function() {
 
   console.log('listening on *:3000');
 
   gps.on('data', function() {
+    if (prev.lat !== null && prev.lon !== null) {
+      gps.state.bearing = GPS.Heading(prev.lat, prev.lon, gps.state.lat, gps.state.lon);
+    }
     io.emit('state', gps.state);
+    prev.lat = gps.state.lat;
+    prev.lon = gps.state.lon;
+    ;
   });
 
   parser.on('data', function(data) {
